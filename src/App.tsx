47d627859dfcf2e1f669bd458c24a794afeac42b
mdsabbir1001 <brokenheart.badman@gmail.com>
2025-07-03 16:16:33 +0600

    Fix React Router nested routes for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard/*" element={<DashboardPage />} />
         <Route path="*" element={<LoginPage />} /> {/* Default to login page */}
       </Routes>
     </Router>
